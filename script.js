2955f5ddbabb6d0f991c8b1c279d35802ca31201
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2025-03-06 22:03:08 +0300

    adding questions array that contain question and answer

---
+++ script.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question:"what is the capital city of Ethiopia",
+        answer:[
+            {text:"Cairo",correct:false},
+            {text:"London",correct:false},
+            {text:"Addis Ababa",correct:true},
+            {text:"Madrid",correct:false}
+        ]
+    },
+    {
+        question:"Which one of the following is not a browser",
+        answer:[
+            {text:"Chrome",correct:false},
+            {text:"Microsoft edge",correct:false},
+            {text:"Opera",correct:false},
+            {text:"HTML",correct:true}
+        ]
+    },
+    {
+        question:"what is the largest animal",
+        answer:[
+            {text:"Whale",correct:false},
+            {text:"Elephant",correct:false},
+            {text:"Cat",correct:true},
+            {text:"Dog",correct:false}
+        ]
+    }
+];
